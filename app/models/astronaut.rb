3f9da7d41f1843e8d6b50138860f54da5ed4fba9
Tylor Schafer <tylorschafer@gmail.com>
2019-08-23 09:54:09 -0600

    Add testing for astronaught missions list

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -2,4 +2,8 @@ class Astronaut < ApplicationRecord
   has_many :astronaut_missions
   has_many :missions, through: :astronaut_missions
   validates_presence_of :name, :age, :job
+
+  def self.average_age
+    Astronaut.average(:age)
+  end
 end
--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -6,20 +6,46 @@ describe 'astronauts index page' do
     @moe = Astronaut.create!(name: 'Moe', age: 33, job: 'Engineer')
     @joe = Astronaut.create!(name: 'Joe', age: 36, job: 'Physicist')
     @all = [@bob,@moe,@joe]
+
+    @capricorn = @bob.missions.create(title: 'Capricorn 4', time_in_space: 84)
+    @apollo = @bob.missions.create(title: 'Apollo 13', time_in_space: 120)
+    @gemini = @moe.missions.create(title: 'Gemini 7', time_in_space: 134)
+    @moe.missions << @capricorn
+    @joe.missions << @apollo
+    @joe.missions << @gemini
   end
   it "I see a list of astronauts and their info" do
     visit '/astronauts'
 
     @all.each do |naut|
+      within "#naut-#{naut.id}" do
         expect(page).to have_content(naut.name)
         expect(page).to have_content(naut.age)
         expect(page).to have_content(naut.job)
+      end
     end
   end
 
   it "I can see the average age of all astronauts" do
     visit '/astronauts'
 
-    expect(page).to have_content("Average age: 38")
+    expect(page).to have_content("Average Age: 38")
+  end
+
+  it "I see a list of space missions in alphabetical order for each astronaut." do
+    visit '/astronauts'
+
+    within "#naut-#{@bob.id}" do
+      expect(page).to have_content("Missions: Apollo 13, Capricorn 4")
+    end
+
+    within "#naut-#{@moe.id}" do
+      expect(page).to have_content("Missions: Capricorn 4, Gemini 7")
+    end
+
+    within "#naut-#{@joe.id}" do
+      expect(page).to have_content("Missions: Apollo 13, Gemini 7")
+    end
+
   end
 end
